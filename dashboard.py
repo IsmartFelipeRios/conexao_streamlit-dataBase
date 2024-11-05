a1a30353c085a7dbe5d4cf71db4cfd1e78a1ff25
IsmartFelipeRios <felipe.amaral@ismart.org.br>
2024-11-05 17:24:17 -0300

    Atualização em posicao das funcoes

--- dashboard.py
+++ dashboard.py
@@ -2,11 +2,10 @@ import streamlit as st
 import pyodbc
 import pandas as pd
 
-@st.cache_data()
-def run_query(query):
-    # Conexão e execução da query em uma única função
-    try:
-        conn = pyodbc.connect(
+def make_df(query):
+    @st.cache_resource
+    def init_connection():
+        return pyodbc.connect(
             "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
             + st.secrets["server"]
             + ";DATABASE="
@@ -16,23 +15,30 @@ def run_query(query):
             + ";PWD="
             + st.secrets["password"]
         )
-        df = pd.read_sql_query(query, conn)
-    except Exception as e:
-        st.error(f'Erro ao conectar ou executar a query: {e}')
-        df = pd.DataFrame()
-    finally:
-        conn.close()
-    return df
 
+    @st.cache_data()
+    def run_query(query):
+        try:
+            conn = init_connection()
+        except Exception as e:
+            st.error(f'Erro ao conectar: {e}')
+
+        df = pd.read_sql_query(query, conn)
+        return df
+    run_query(query)
+    
 query = st.text_input('SQL query')
 
 if query:
-    df = run_query(query)
-    if not df.empty:
-        st.dataframe(df)
-    else:
-        st.warning('A consulta não retornou resultados.')
-else:
-    st.warning('Coloque a query na caixa')
+    try:
+        df = make_df(query)
+    except Exception as e:
+        st.error(f'Erro com a query: {e}')
+    
+    # Print results.
+    st.dataframe(df)
 
-st.button("Rerun")
+else: st.warning('Coloque a query na caixa')
+
+st.button("rerun")
+    
