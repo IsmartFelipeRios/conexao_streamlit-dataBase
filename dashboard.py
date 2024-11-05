358887899467885c87e71ad3bddd3cbe0a64ec6f
IsmartFelipeRios <felipe.amaral@ismart.org.br>
2024-11-05 17:08:42 -0300

    Fazendo funcao unica

--- dashboard.py
+++ dashboard.py
@@ -2,35 +2,35 @@ import streamlit as st
 import pyodbc
 import pandas as pd
 
+def make_df(query):
+    @st.cache_resource
+    def init_connection():
+        return pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+            + st.secrets["server"]
+            + ";DATABASE="
+            + st.secrets["database"]
+            + ";UID="
+            + st.secrets["username"]
+            + ";PWD="
+            + st.secrets["password"]
+        )
 
-@st.cache_resource
-def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-    )
+    @st.cache_data()
+    def run_query(query):
+        try:
+            conn = init_connection()
+        except Exception as e:
+            st.error(f'Erro ao conectar: {e}')
 
-@st.cache_data()
-def run_query(query):
-    try:
-        conn = init_connection()
-    except Exception as e:
-        st.error(f'Erro ao conectar: {e}')
-
-    df = pd.read_sql_query(query, conn)
-    return df
+        df = pd.read_sql_query(query, conn)
+        return df
 
 query = st.text_input('SQL query')
 
 if query:
     try:
-        df = run_query(query)
+        df = make_df(query)
     except Exception as e:
         st.error(f'Erro com a query: {e}')
     
